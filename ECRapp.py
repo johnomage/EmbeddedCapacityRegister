2e673baef6378fa953f3e391c9f1459ca1eb725c
johnomage <31286651+johnomage@users.noreply.github.com>
2024-09-19 17:01:57 +0100

    updated notebook

---
+++ ECRapp.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+import geopandas as gpd
+import os, ast
+from dotenv import load_dotenv
+
+# WIP
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+
+
+
+
+
+Latest Embedded Capacity Regiister: [Embedded Capacity Regiister](https://www.nationalgrid.co.uk/our-network/embedded-capacity-register)
